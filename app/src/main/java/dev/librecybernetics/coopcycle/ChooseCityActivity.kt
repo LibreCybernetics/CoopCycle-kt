6c706c08bc60dd20e7ae5d92deaf6762ef8bc191
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2021-08-25 15:58:36 -0500

    Create ChooseCity Activity

--- app/src/main/java/dev/librecybernetics/coopcycle/ChooseCityActivity.kt
+++ app/src/main/java/dev/librecybernetics/coopcycle/ChooseCityActivity.kt
@@ -3,9 +3,9 @@ package dev.librecybernetics.coopcycle
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ChooseCityActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_choosecity)
     }
 }
