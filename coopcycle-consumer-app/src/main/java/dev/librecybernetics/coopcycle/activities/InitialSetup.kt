7b6cdada28a169cdf0124ddac60129d32a2981d4
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2021-08-28 11:43:03 -0500

    Refactor towards Declarative-KotlinMultiPlatform

--- coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/activities/InitialSetup.kt
+++ coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/activities/InitialSetup.kt
@@ -7,50 +7,69 @@ import android.os.Bundle
 import android.util.Log
 import android.view.KeyEvent
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
+import androidx.compose.ui.platform.*
+import androidx.compose.ui.unit.*
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 import dev.librecybernetics.coopcycle.R
 import dev.librecybernetics.coopcycle.dao.CooperativeSummaryDAO
 import dev.librecybernetics.coopcycle.schema.Cooperative
+import dev.librecybernetics.coopcycle.types.CooperativeName
 import dev.librecybernetics.coopcycle.util.location
 import dev.librecybernetics.location.LocationActivityService
-import kotlin.math.max
+import dev.librecybernetics.types.CityName
+import dev.librecybernetics.types.CountryCode
+import kotlinx.coroutines.flow.*
 
 class InitialSetup : AppCompatActivity(), LocationActivityService, CooperativeSummaryDAO {
     companion object {
         private enum class Screen { Welcome, Selection }
 
-        private data class CooperativeHolder(val view: View) : RecyclerView.ViewHolder(view) {
-            init {
-                view.setOnClickListener {
-                    Toast.makeText(
-                        view.context,
-                        "Selected ${currentCooperative?.name}",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
+        private data class CooperativeHolder(val view: ComposeView) :
+            RecyclerView.ViewHolder(view) {
+            private lateinit var currentCooperative: Cooperative
 
-            private val cityNameView: TextView = view.findViewById(R.id.choose_city_card_city)
-            private val countryCodeView: TextView =
-                view.findViewById(R.id.choose_city_card_country_code)
-            private val cooperativeNameView: TextView =
-                view.findViewById(R.id.choose_city_card_coop_name)
-
-            private var currentCooperative: Cooperative? = null
+            @ExperimentalMaterialApi
             fun updateWithCooperative(cooperative: Cooperative) {
                 currentCooperative = cooperative
-                cityNameView.text = cooperative.city.name
-                countryCodeView.text = cooperative.country.code
-                cooperativeNameView.text = cooperative.name.name
+                view.setContent {
+                    CooperativeCard(
+                        cooperative.city,
+                        cooperative.name,
+                        cooperative.country
+                    )
+                }
+            }
+        }
+
+        @Composable
+        @ExperimentalMaterialApi
+        fun CooperativeCard(
+            cityName: CityName,
+            cooperativeName: CooperativeName,
+            countryCode: CountryCode
+        ) {
+            Card(onClick = { }, elevation = 10.dp) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(20.dp, 5.dp)
+                ) {
+                    Column {
+                        Text(cityName.name)
+                        Text(cooperativeName.name)
+                    }
+                    Spacer(Modifier.weight(1f))
+                    Text(countryCode.code)
+                }
             }
         }
 
@@ -63,9 +82,11 @@ class InitialSetup : AppCompatActivity(), LocationActivityService, CooperativeSu
                     LayoutInflater
                         .from(parent.context)
                         .inflate(R.layout.initial_setup_cooperative_card, parent, false)
+                            as ComposeView
                 )
             }
 
+            @ExperimentalMaterialApi
             override fun onBindViewHolder(holder: CooperativeHolder, position: Int) {
                 holder.updateWithCooperative(sortedCooperatives[position])
             }
@@ -77,13 +98,27 @@ class InitialSetup : AppCompatActivity(), LocationActivityService, CooperativeSu
     }
 
     // TODO: Move to Bundle to cache results
-    private var cooperatives: Set<Cooperative> = setOf()
+    override val cooperatives: MutableStateFlow<Set<Cooperative>> = MutableStateFlow(setOf())
     private var currentScreen: Screen = Screen.Welcome
 
     override val activity: Activity = this
     override lateinit var locationManager: LocationManager
     override lateinit var requestQueue: RequestQueue
 
+    @Composable
+    @ExperimentalMaterialApi
+    private fun CooperativesList(
+        cooperatives: StateFlow<Set<Cooperative>>
+    ) {
+        val observedCooperatives by cooperatives.collectAsState()
+        LazyColumn {
+            items(observedCooperatives.sortedBy { it.city.name }) { cooperative: Cooperative ->
+                CooperativeCard(cooperative.city, cooperative.name, cooperative.country)
+            }
+        }
+    }
+
+    @ExperimentalMaterialApi
     private fun changeScreen(toScreen: Screen) {
         when (toScreen) {
             Screen.Welcome -> {
@@ -93,42 +128,41 @@ class InitialSetup : AppCompatActivity(), LocationActivityService, CooperativeSu
             }
             Screen.Selection -> {
                 setContentView(R.layout.initial_setup_selection)
-                // Reload page with results; TODO: Investigate graceful recyclerview update
-                cooperatives = fetchCooperatives().orEmpty()
-                if (cooperatives.isNotEmpty()) {
-                    val chooseCityRecyclerView: RecyclerView =
-                        findViewById(R.id.choose_city_recycler_view)
-                    chooseCityRecyclerView.adapter = CooperativeCardRecyclerAdapter(cooperatives)
-                    chooseCityRecyclerView.layoutManager = LinearLayoutManager(this)
-                    val coarseLocation: Location? = getLocation(
-                        LocationActivityService.Companion.LocationFreshness.Any,
-                        LocationActivityService.Companion.LocationAccuracy.Coarse
-                    )
-                    Log.d("SELECT.COOPERATIVES.LOCATION", coarseLocation.toString())
-                    val closestCoop: Cooperative? = cooperatives.sortedBy {
-                        coarseLocation?.distanceTo(location(it))
-                    }.getOrNull(0)
-                    Log.d("SELECT.COOPERATIVES.CLOSEST", closestCoop.toString())
-                    if (closestCoop != null) {
-                        val position = cooperatives.sortedBy { it.city.name }
-                            .indexOfFirst { it.city.name == closestCoop.city.name }
-                        chooseCityRecyclerView.scrollToPosition(max(position - 3, 0))
-                    }
+                val chooseCityComposeView: ComposeView =
+                    findViewById(R.id.choose_city_compose_view)
+                chooseCityComposeView.setContent {
+                    CooperativesList(cooperatives)
                 }
+                val coarseLocation: Location? = getLocation(
+                    LocationActivityService.Companion.LocationFreshness.Any,
+                    LocationActivityService.Companion.LocationAccuracy.Coarse
+                )
+                Log.d("SELECT.COOPERATIVES.LOCATION", coarseLocation.toString())
+                val closestCoop: Cooperative? = cooperatives.value.sortedBy {
+                    coarseLocation?.distanceTo(location(it))
+                }.getOrNull(0)
+                Log.d("SELECT.COOPERATIVES.CLOSEST", closestCoop.toString())
+//                    if (closestCoop != null) {
+//                        val position = cooperatives.sortedBy { it.city.name }
+//                            .indexOfFirst { it.city.name == closestCoop.city.name }
+//                        chooseCityComposeView.scrollToPosition(max(position - 3, 0))
+//                    }
             }
         }
         currentScreen = toScreen
     }
 
+    @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         changeScreen(Screen.Welcome)
 
         locationManager = getSystemService(LocationManager::class.java)
         requestQueue = Volley.newRequestQueue(this)
-        fetchCooperatives() // Prefetch before they are needed
+        requestCooperatives() // Prefetch before they are needed
     }
 
+    @ExperimentalMaterialApi
     override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
         return when (keyCode) {
             KeyEvent.KEYCODE_BACK -> when (currentScreen) {
--- coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/dao/CooperativeSummaryDAO.kt
+++ coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/dao/CooperativeSummaryDAO.kt
@@ -2,68 +2,53 @@ package dev.librecybernetics.coopcycle.dao
 
 import android.app.Activity
 import android.util.Log
-import android.widget.Toast
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import dev.librecybernetics.coopcycle.schema.Cooperative
 import dev.librecybernetics.coopcycle.util.UTF8StringRequest
+import kotlinx.coroutines.flow.*
 
 interface CooperativeSummaryDAO {
     companion object {
         sealed interface Result
-        data class Success(val cooperatives: Set<Cooperative>) : Result
+        object Success : Result
         data class Error(val error: VolleyError) : Result
 
         var result: Result? = null
-        private val cooperativeSummaryRequest: StringRequest = UTF8StringRequest(
+    }
+
+    val cooperatives: MutableStateFlow<Set<Cooperative>>
+    val requestQueue: RequestQueue
+    val activity: Activity
+
+    fun requestCooperatives() {
+        fun processError(error: VolleyError) {
+            result = Error(error)
+            Log.e("FETCH.COOPERATIVES", error.message.orEmpty())
+        }
+
+        fun processResponse(response: String) {
+            val newData = Cooperative.setFromJSONString(response)
+            if (newData.isEmpty()) {
+                Log.w("FETCH.COOPERATIVES", "Successfully got zero cooperatives")
+            } else {
+                Log.i("FETCH.COOPERATIVES", "Successfully got list of cooperatives")
+                Log.v("FETCH.COOPERATIVES", newData.map { it.name }.toString())
+            }
+            cooperatives.update { current -> current + newData }
+            result = Success
+        }
+
+        val cooperativeSummaryRequest: StringRequest = UTF8StringRequest(
             Request.Method.GET,
             "https://coopcycle.org/coopcycle.json",
             { processResponse(it) },
             { processError(it) }
         )
 
-        private fun processError(error: VolleyError) {
-            result = Error(error)
-            Log.e("FETCH.COOPERATIVES", error.message.orEmpty())
-        }
-
-        private fun processResponse(response: String) {
-            result = Success(Cooperative.setFromJSONString(response))
-        }
-    }
-
-    val requestQueue: RequestQueue
-    val activity: Activity
-
-    private fun sendRequest() {
         requestQueue.add(cooperativeSummaryRequest)
         requestQueue.start()
-    }
-
-    fun fetchCooperatives(): Set<Cooperative>? {
-        return when (result) {
-            is Error -> {
-                val error = (result as Error).error
-                Toast.makeText(activity, error.localizedMessage, Toast.LENGTH_LONG).show()
-                sendRequest()
-                null
-            }
-            is Success -> {
-                val cooperatives = (result as Success).cooperatives
-                if (cooperatives.isEmpty()) {
-                    Log.w("FETCH.COOPERATIVES", "Successfully got zero cooperatives")
-                    null
-                } else {
-                    Log.i("FETCH.COOPERATIVES", "Successfully got list of cooperatives")
-                    Log.v("FETCH.COOPERATIVES", cooperatives.map { it.name }.toString())
-                    cooperatives
-                }
-            }
-            null -> {
-                sendRequest(); null
-            }
-        }
     }
 }
