cebdc9cf752b0e697841ee4529261a78e2314dd6
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2021-08-29 21:07:00 -0500

    Add Scaffold

--- coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/activities/MainActivity.kt
+++ coopcycle-consumer-app/src/main/java/dev/librecybernetics/coopcycle/activities/MainActivity.kt
@@ -5,9 +5,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.*
+import androidx.compose.material.icons.filled.*
+import androidx.compose.ui.graphics.*
+import dev.librecybernetics.coopcycle.R
 import dev.librecybernetics.location.LocationActivityService
 
+@ExperimentalMaterialApi
 class MainActivity : AppCompatActivity(), LocationActivityService {
     override val activity: ComponentActivity = this
     override lateinit var locationManager: LocationManager
@@ -15,10 +21,23 @@ class MainActivity : AppCompatActivity(), LocationActivityService {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent { 
-            Scaffold() {
-                
+        setContent {
+            Scaffold(
+                topBar = {
+                    TopAppBar(
+                        backgroundColor = Color(getColor(R.color.coopcycle_red))
+                    ) {
+                            Icon(Icons.Default.Menu, "Side Menu", tint = MaterialTheme.colors.surface)
+                    }
+                },
+                drawerContent = {
+                    Column {
+                        Text("Drawer")
+                    }
+                }
+            ) {
+                Text("MainActivity")
             }
-            Text("MainActivity") }
+        }
     }
 }
